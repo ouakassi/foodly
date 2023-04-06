07c6ac24a12ff6c611a78d8c357189004118b800
ouakassi <lmoutchoo@gmail.com>
2023-04-06 14:04:01 +0000

    refactor dahsboard sidebar

--- client/src/components/Dashboard/DashboardSidebar.jsx
+++ client/src/components/Dashboard/DashboardSidebar.jsx
@@ -7,74 +7,105 @@ import {
   CiMoneyCheck1,
   CiUser,
   CiSun,
-  CiReceipt,
-  CiMenuBurger,
 } from "react-icons/ci";
-import { BsArrowLeftCircle } from "react-icons/bs";
-import { useState } from "react";
+import {
+  TbArrowAutofitContent,
+  TbArrowAutofitContentFilled,
+} from "react-icons/tb";
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import AnimatedIcon from "../Buttons/AnimatedIcon";
+
+const navItems = [
+  {
+    icon: <CiGrid41 />,
+    name: "overview",
+  },
+  {
+    icon: <CiMoneyCheck1 />,
+    name: "orders",
+  },
+  {
+    icon: <CiUser />,
+    name: "customers",
+  },
+  {
+    icon: <CiShoppingCart />,
+    name: "products",
+  },
+  {
+    icon: <CiMemoPad />,
+    name: "reports",
+  },
+];
 
 export default function DashboardSidebar() {
   const [toggleSidebar, setToggleSidebar] = useState(false);
+
+  useEffect(() => {
+    const spanElements = document.querySelectorAll(".dashboard__sidebar-text");
+    spanElements.forEach(
+      (span) => (span.style.display = toggleSidebar ? "none" : "block")
+    );
+  }, [toggleSidebar]);
+
   return (
-    <div className="dashboard__sidebar">
-      <button onClick={() => {}} className="dashboard__sidebar-button-toggle">
-        <BsArrowLeftCircle />
-      </button>
+    <motion.div
+      animate={!toggleSidebar ? { width: "22rem" } : { width: "9rem" }}
+      className="dashboard__sidebar"
+    >
       <header className="dashboard__sidebar-header">
+        <AnimatedIcon
+          onClick={() => setToggleSidebar(!toggleSidebar)}
+          scale={1.1}
+          className="dashboard__sidebar-button-toggle"
+          icon={
+            toggleSidebar ? (
+              <TbArrowAutofitContent />
+            ) : (
+              <TbArrowAutofitContentFilled />
+            )
+          }
+        />
         <Logo
           link={"/dashboard"}
-          //   logoName={"Foodly"}
+          logoName={!toggleSidebar && "Foodly"}
           className={"dashboard__sidebar-logo"}
         />
       </header>
 
       <main className="dashboard__sidebar-main">
-        <h3>menu</h3>
+        {/* <h3>menu</h3> */}
         <ul className="dashboard__sidebar-navlist ">
-          <li className="dashboard__sidebar-navitem active">
-            <a href="#d" className="dashboard__sidebar-navlink ">
-              <CiGrid41 className="dashboard__sidebar-icon" />
-              <span className="dashboard__sidebar-text">overview</span>
-            </a>
-          </li>
-          <li className="dashboard__sidebar-navitem">
-            <a href="#d" className="dashboard__sidebar-navlink">
-              <CiMoneyCheck1 className="dashboard__sidebar-icon" />
-              <span className="dashboard__sidebar-text">orders</span>
-            </a>
-          </li>
-          <li className="dashboard__sidebar-navitem">
-            <a href="#d" className="dashboard__sidebar-navlink">
-              <CiUser className="dashboard__sidebar-icon" />
-              <span className="dashboard__sidebar-text">custumers</span>
-            </a>
-          </li>
-          <li className="dashboard__sidebar-navitem">
-            <a href="#d" className="dashboard__sidebar-navlink">
-              <CiShoppingCart className="dashboard__sidebar-icon" />
-              <span className="dashboard__sidebar-text">products</span>
-            </a>
-          </li>
-
-          <li className="dashboard__sidebar-navitem">
-            <a href="#d" className="dashboard__sidebar-navlink">
-              <CiMemoPad className="dashboard__sidebar-icon" />
-              <span className="dashboard__sidebar-text">reports</span>
-            </a>
-          </li>
+          {navItems.map(({ icon, name }, i) => {
+            return (
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active" : "dashboard__sidebar-navlink"
+                }
+                to={name}
+              >
+                <li key={i} className="dashboard__sidebar-navitem">
+                  <span className="dashboard__sidebar-icon">{icon}</span>
+                  <span className="dashboard__sidebar-text">{name}</span>
+                </li>{" "}
+              </NavLink>
+            );
+          })}
         </ul>
       </main>
       <footer>
-        <h3>admin settings</h3>
+        {/* <h3>admin settings</h3> */}
         <ul className="dashboard__sidebar-navlist">
-          <li className="dashboard__sidebar-navitem">
-            <a href="#d" className="dashboard__sidebar-navlink">
+          <a href="#d" className="dashboard__sidebar-navlink">
+            <li className="dashboard__sidebar-navitem">
               <CiSun className="dashboard__sidebar-icon" />
               <span className="dashboard__sidebar-text">settings</span>
-            </a>
-          </li>
+            </li>
+          </a>
         </ul>
       </footer>
-    </div>
+    </motion.div>
   );
 }
