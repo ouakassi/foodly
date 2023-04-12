72361936d6d21d92563f10f1f97d335597b25612
ouakassi <lmoutchoo@gmail.com>
2023-04-12 16:09:16 +0000

    refactor root layout

--- client/src/pages/RootLayout.jsx
+++ client/src/pages/RootLayout.jsx
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Navigation/Header";
@@ -8,41 +6,21 @@ import Header from "../components/Navigation/Header";
 import ScrollToTop from "../hooks/useScrollToTop";
 
 export default function RootLayout() {
-  const [show, setShow] = useState(false);
-
-  const pathName = useLocation().pathname.slice(1);
-
   const mainStyle = {
-    paddingTop: show && "var(--header-height)",
+    paddingTop: "var(--header-height)",
     maxWidth: "1920px",
     minHeight: "calc(80vh - var(--header-height) )",
     margin: "0 auto",
   };
 
-  useEffect(() => {
-    const locations = [
-      "auth/login",
-      "auth/register",
-      "auth/forgot-password",
-      "auth/reset-password",
-      "auth/email-sent",
-      "dashboard",
-    ];
-    if (locations.includes(pathName)) {
-      setShow(false);
-    } else {
-      setShow(true);
-    }
-  }, [pathName]);
-
   return (
     <>
-      {show && <Header />}
+      <Header />
       <ScrollToTop />
       <main style={mainStyle}>
         <Outlet />
       </main>
-      {show && <Footer />}
+      <Footer />
     </>
   );
 }
