b9982e24c0dbe885cbd699168217e750b1e0388e
ouakassi <lmoutchoo@gmail.com>
2023-04-12 16:10:01 +0000

    refactor app layouts

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
+  Navigate,
 } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 
@@ -11,15 +12,27 @@ import "./styles/global.css";
 import "./styles/layout.css";
 
 import RootLayout from "./pages/RootLayout";
-import HomePage from "./pages/Home/HomePage";
+
+// auth
 import LoginPage from "./pages/Authentication/LoginPage";
 import RegisterPage from "./pages/Authentication/RegisterPage";
 import ForgotPasswordPage from "./pages/Authentication/ForgotPasswordPage";
 import ResetPassword from "./pages/Authentication/ResetPassword";
 import EmailSent from "./pages/Authentication/EmailSent";
+
+// other
+import HomePage from "./pages/Home/HomePage";
 import AboutPage from "./pages/About/AboutPage";
 import StorePage from "./pages/Store/StorePage";
-import DashboardPage from "./pages/Dashboard/DashboardPage";
+
+// dashboard
+import DashboardLayout from "./pages/DashboardLayout";
+import OverviewPage from "./pages/Dashboard/OverviewPage";
+import CustomersPage from "./pages/Dashboard/CustomersPage";
+import OrdersPage from "./pages/Dashboard/OrdersPage";
+import ProductsPage from "./pages/Dashboard/ProductsPage";
+import ReportsPage from "./pages/Dashboard/ReportsPage";
+import SettingsPage from "./pages/Dashboard/SettingsPage";
 
 const LazyAbout = lazy(() => import("./pages/About/AboutPage"));
 
@@ -37,10 +50,7 @@ const authRoutes = [
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="store" element={<StorePage />}></Route>
-        <Route path="dashboard" element={<DashboardPage />}></Route>
+      <>
         <Route
           path="about"
           element={
@@ -48,21 +58,27 @@ function App() {
               <LazyAbout />
             </Suspense>
           }
-        ></Route>
-
-        {/* <Route path="cart" element={<div>cart</div>} />
-        <Route path="oils" element={<div>oils</div>} />
-        <Route path="nuts" element={<div>nuts</div>} />
-        <Route path="herbs" element={<div>herbs</div>} /> */}
-        <Route path="sale" element={<div>sale</div>} />
-        <Route path="recipes" element={<div>recipes</div>} />
-        <Route path="*" element={<div>sorry no page here</div>} />
+        />
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="sale" element={<div>sale</div>} />
+          <Route path="recipes" element={<div>recipes</div>} />
+          <Route path="*" element={<div>sorry no page here</div>} />
+        </Route>
+        <Route path="dashboard" element={<DashboardLayout />}>
+          <Route index path="overview" element={<OverviewPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+          <Route path="customers" element={<CustomersPage />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="reports" element={<ReportsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
         <Route path="auth">
           {authRoutes.map(({ path, element }, key) => {
             return <Route key={key} path={path} element={element} />;
           })}
         </Route>
-      </Route>
+      </>
     )
   );
 
