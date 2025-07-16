9b8166a3bb8d0a6f6aec565ecf58d235793385e6
oussama ouakassi <lmoutchoo@gmail.com>
2025-07-16 22:15:56 +0100

    refactor: update LoadingSpinner component to accept style prop and simplify props handling; adjust padding in CustomButton CSS

--- client/src/components/Forms/LoadingSpinner.jsx
+++ client/src/components/Forms/LoadingSpinner.jsx
@@ -1,10 +1,5 @@
 import "./LoadingSpinner.css";
 
-export default function LoadingSpinner({ height, width, borderWidth }) {
-  return (
-    <span
-      style={{ height: height, width: width, borderWidth: borderWidth }}
-      className="loader"
-    ></span>
-  );
+export default function LoadingSpinner({ style }) {
+  return <span style={style} className="loader"></span>;
 }
