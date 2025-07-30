8a8097ed64cee70a32e88117c9aea7ee1324f601
oussama ouakassi <lmoutchoo@gmail.com>
2025-07-30 16:49:44 +0100

    feat: update Product model with new attributes, validation, and soft delete support

--- server/src/models/productModel.js
+++ server/src/models/productModel.js
@@ -1,55 +1,82 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../utils/database.js";
+import { generateSlug } from "../utils/model.js";
+import { PRODUCT_STATUS_VALUES_ARRAY } from "../utils/constants.js";
 
-const Product = sequelize.define("product", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  imgUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-    validate: {
-      min: 0,
+const Product = sequelize.define(
+  "product",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    imgUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    basePrice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      validate: {
+        min: 0,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        min: 0,
+      },
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: PRODUCT_STATUS_VALUES_ARRAY[0], // Default to "active"
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        len: {
+          args: [0, 1000],
+          msg: "Description cannot exceed 1000 characters",
+        },
+      },
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isLowercase: true,
+        is: {
+          args: /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
+          msg: "Slug must be lowercase with hyphens only",
+        },
+      },
     },
   },
-  status: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
-    allowNull: false,
-  },
-  stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
-  discount: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-    defaultValue: 0,
-    validate: {
-      min: 0,
-      max: 100,
+  {
+    paranoid: true, // Soft deletes
+    hooks: {
+      beforeCreate: async (product) => {
+        if (!product.slug) {
+          product.slug = generateSlug(product.name);
+        }
+      },
+      beforeUpdate: async (product) => {
+        if (product.changed("name") && !product.changed("slug")) {
+          product.slug = generateSlug(product.name);
+        }
+      },
     },
-  },
-  category: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  isDeleted: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-});
+  }
+);
 
 export default Product;
